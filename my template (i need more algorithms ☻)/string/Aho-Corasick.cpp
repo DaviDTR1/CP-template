4be9360e8cd7d36a260a892c33d02dc6a41d9296
DaviDTR1 <102010444+DaviDTR1@users.noreply.github.com>
2022-03-27 00:36:42 +0100

    Update Aho-Corasick.cpp

--- my template (i need more algorithms ☻)/string/Aho-Corasick.cpp
+++ my template (i need more algorithms ☻)/string/Aho-Corasick.cpp
@@ -1,1 +1,72 @@
 ///Aho Corasick
+struct vertex{
+    ll next[K];
+    ll suffix = 0;
+    bool leaf = 0;
+    vertex(){
+        fill(begin(next), end(next),-1);
+    }
+};
+vector<vertex> trie(1);
+vi order;
+vi sol;
+vi nod;
+
+void add_string(string const& s,ll id){
+    ll v = 0;
+    rep(u, s){
+        int c = u-'A';
+        if(trie[v].next[c] == -1){
+            trie[v].next[c] = trie.size();
+            trie.emplace_back();
+            sol.pb(0);
+        }
+        v = trie[v].next[c];
+    }
+    trie[v].leaf = 1;
+    nod[id] = v;
+
+}
+
+void buildAHO(){
+    order.clear();
+    ll v = 0;
+    queue<ll>qp;
+    fr(i,0,K-1){
+        if(trie[v].next[i]!=-1){
+            trie[v].suffix = v;
+            qp.push(trie[v].next[i]);
+        } else{
+            trie[v].next[i] = v;
+        }
+    }
+    while(!qp.empty()){
+        v = qp.front();
+        qp.pop();
+        order.pb(v);
+        fr(i,0,K-1){
+            ll u = trie[v].next[i];
+            if(u != -1){
+                trie[u].suffix = trie[trie[v].suffix].next[i];
+                qp.push(u);
+            } else{
+                trie[v].next[i] = trie[trie[v].suffix].next[i];
+            }
+        }
+
+    }
+}
+///move through the text and count the number of times I land on each position
+void move(string &a){
+    ll v = 0;
+    rep(u, a){
+        ll c = u-'A';
+        v = trie[v].next[c];
+        sol[v]++;
+    }
+    ll ta = order.size();
+    for(ll i = ta-1; i>=0;i--){
+        v = order[i];
+        sol[trie[v].suffix] += sol[v];
+    }
+}
