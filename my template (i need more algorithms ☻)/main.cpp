98a29dff485aa0d07005e139837878df2503847a
josuefernandezpalomo <fernandezpalomojosue@gmail.com>
2024-10-04 15:49:56 -0400

    Update main.cpp
    Template ajustado

--- my template (i need more algorithms ☻)/main.cpp
+++ my template (i need more algorithms ☻)/main.cpp
@@ -9,35 +9,37 @@ typedef tree<int,null_type, less<int>,rb_tree_tag,
         tree_order_statistics_node_update> indexed_set;
 */
 #define pb push_back
+#define int long long
 #define ll long long
-#define x1 first
-#define y1 second
-#define frb(i,a,b) for(ll (i)=(a);(i) <= (b); (i)+=(i)&-(i))
-#define rfrb(i,a) for(ll (i)=(a);(i) > 0;(i)-=(i)&-(i))
-#define fr(i,a,b) for(ll (i) = (a); (i) <= (b); (i)++)
+#define dd long double
+#define fst first
+#define snd second
+#define frb(i,a,b) for(int (i)=(a);(i) <= (b); (i)+=(i)&-(i))
+#define rfrb(i,a) for(int (i)=(a);(i) > 0;(i)-=(i)&-(i))
+#define fr(i,a,b) for(int (i) = (a); (i) <= (b); (i)++)
 #define rep(i,c) for(auto (i) : (c))
 #define mini(a,b) (((a) < (b)) ? (a) : (b))
 #define maxi(a,b) (((a) > (b)) ? (a) : (b))
-#define par pair<ll,ll>
+#define par pair<int,int>
 #define vp vector<par>
-#define vi vector<ll>
-#define dq deque<ll>
-#define mem(i,j) memset((i),(j),sizeof (i))
-//indexed_set s; 
+#define vi vector<int>
+#define dq deque<int>
+
+//indexed_set s;
 ///always see the properties of the problems
 ///siempre poner ("\n" cout.flush) como endl
 ///compare doubles a,b fabs(a-b)<eps
 ///----------------------------------------------------------------------------
 
-const ll mod = 1e9+7;
-const ll INF = 1e18;
-ll n,t;
-     
-int main() {
+const int mod = 1e9+7;
+const int INF = 1e18;
+const dd eps=1e-9;s
+int n,t;
+
+signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
     cout.setf(ios::fixed);cout.precision(0);
-    ///freopen("a.in","r",stdin);
-    ///freopen("a.out","w",stdout);
+
     cin>>n;
     return 0;
 }
