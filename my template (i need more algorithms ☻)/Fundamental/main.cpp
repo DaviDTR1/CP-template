f888a4ca95550984c25ff0959ad9d16750f858a9
josuefernandezpalomo <fernandezpalomojosue@gmail.com>
2024-10-04 21:53:16 -0400

    reorganizando
    reorganizando

---
+++ my template (i need more algorithms ☻)/Fundamental/main.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+*/
+using namespace std;
+/*
+typedef tree<int,null_type, less<int>,rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+*/
+#define pb push_back
+#define int long long
+#define ll long long
+#define dd long double
+#define fst first
+#define snd second
+#define frb(i,a,b) for(int (i)=(a);(i) <= (b); (i)+=(i)&-(i))
+#define rfrb(i,a) for(int (i)=(a);(i) > 0;(i)-=(i)&-(i))
+#define fr(i,a,b) for(int (i) = (a); (i) <= (b); (i)++)
+#define rep(i,c) for(auto (i) : (c))
+#define mini(a,b) (((a) < (b)) ? (a) : (b))
+#define maxi(a,b) (((a) > (b)) ? (a) : (b))
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)v.size()
+#define par pair<int,int>
+#define vp vector<par>
+#define vi vector<int>
+#define dq deque<int>
+
+//indexed_set s;
+///always see the properties of the problems
+///siempre poner ("\n" cout.flush) como endl
+///compare doubles a,b fabs(a-b)<eps
+///----------------------------------------------------------------------------
+
+const int mod = 1e9+7;
+const int INF = 1e18;
+const dd eps=1e-9;s
+int n,t;
+
+signed main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cout.setf(ios::fixed);cout.precision(0);
+
+    cin>>n;
+    return 0;
+}
